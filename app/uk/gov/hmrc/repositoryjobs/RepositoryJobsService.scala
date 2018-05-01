3f31a9efd78810ec579d9f07ac98a88a8311b787
Dario Abdulrehman <dario.rehman@gmail.com>
2018-05-01 17:19:05 +0100

    PLATOPS-1353: Change service so it persists all the builds fetched from Jenkins

--- app/uk/gov/hmrc/repositoryjobs/RepositoryJobsService.scala
+++ app/uk/gov/hmrc/repositoryjobs/RepositoryJobsService.scala
@@ -17,11 +17,8 @@
 package uk.gov.hmrc.repositoryjobs
 
 import javax.inject.{Inject, Singleton}
-
 import play.Logger
 import play.api.libs.json.{Json, OFormat}
-import uk.gov.hmrc.repositoryjobs.config.RepositoryJobsConfig
-
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.Try
@@ -31,13 +28,11 @@ import scala.util.control.NonFatal
 class RepositoryJobsService @Inject()(
   repository: BuildsRepository,
   jenkinsDevConnector: JenkinsCiDevConnector,
-  jenkinsOpenConnector: JenkinsCiOpenConnector,
-  repositoryJobsConfig: RepositoryJobsConfig) {
+  jenkinsOpenConnector: JenkinsCiOpenConnector) {
 
   def update: Future[UpdateResult] = {
     val devBuildsResponseF: Future[JenkinsJobsResponse]  = jenkinsDevConnector.getBuilds
     val openBuildsResponseF: Future[JenkinsJobsResponse] = jenkinsOpenConnector.getBuilds
-    val existingBuildsF: Future[Seq[Build]]              = repository.getAll
 
     Logger.info("Starting repository jobs update")
     (for {
@@ -49,16 +44,9 @@ class RepositoryJobsService @Inject()(
       _ = Logger.info(
         s"Fetched builds from jenkins ci-open. Number of builds: ${openBuildsResponse.jobs.map(_.allBuilds.size).sum}")
 
-      existingBuilds <- existingBuildsF
-      _ = Logger.info(s"Fetched existing repositories from mongo.  Number of existing builds: ${existingBuilds.size}")
-
       allJobs = devBuildsResponse.jobs ++ openBuildsResponse.jobs
 
-      newBuilds = getBuilds(allJobs, existingBuilds)
-      _ = Logger.info(
-        s"Calculated new builds to be saved from jenkins ci-dev and ci-open. Number of new builds: ${newBuilds.size}")
-
-      result <- repository.bulkAdd(newBuilds)
+      result <- repository.persist(convertToBuilds(allJobs))
     } yield result)
       .map { updateResult =>
         Logger.info(s"Completed repository jobs update: $updateResult")
@@ -70,42 +58,25 @@ class RepositoryJobsService @Inject()(
     }
   }
 
-  private[repositoryjobs] def getBuilds(jobs: Seq[Job], existingBuilds: Seq[Build]): Seq[Build] = {
-    val setExistingBuilds = existingBuilds.map(b => (b.jobName, b.timestamp)).toSet
+  private def convertToBuilds(jobs: Seq[Job]): Seq[Build] =
+    jobs.flatMap { job =>
+      val gitUrl   = getGitUrl(job)
+      val repoName = Try(gitUrl.split("/").last.stripSuffix(".git")).toOption
 
-    def buildAlreadyExists(job: Job, buildResponse: BuildResponse): Boolean =
-      setExistingBuilds.contains((job.name, buildResponse.timestamp))
-
-    val jobsWithNewBuilds =
-      jobs.map { (job: Job) =>
-        job.copy(
-          allBuilds = job.allBuilds
-            .filterNot(buildAlreadyExists(job, _))
-            .filter(_.result.isDefined))
+      job.allBuilds.map { buildResponse =>
+        Build(
+          repoName,
+          job.name,
+          job.url,
+          buildResponse.number,
+          buildResponse.result,
+          buildResponse.timestamp,
+          buildResponse.duration,
+          buildResponse.url,
+          buildResponse.builtOn
+        )
       }
-
-    val builds =
-      jobsWithNewBuilds.flatMap { job =>
-        val gitUrl   = getGitUrl(job)
-        val repoName = Try(gitUrl.split("/").last.stripSuffix(".git")).toOption
-
-        job.allBuilds.map { buildResponse =>
-          Build(
-            repoName,
-            job.name,
-            job.url,
-            buildResponse.number,
-            buildResponse.result,
-            buildResponse.timestamp,
-            buildResponse.duration,
-            buildResponse.url,
-            buildResponse.builtOn
-          )
-        }
-      }
-
-    builds
-  }
+    }
 
   private def getGitUrl(job: Job) = job.scm match {
     case Some(scm) => scm.userRemoteConfigs.fold("")(_.head.url.getOrElse(""))
